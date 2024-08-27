89f2ff73d0954cf8930e96e15278984972ac1a48
fuchenxi <1256257282@qq.com>
2024-08-27 10:02:16 +0800

    调整参数位置及移入test

--- elec/__init__.py
+++ elec/__init__.py
@@ -9,7 +9,7 @@ def main(kwargs):
         subprocess.run(['ngspice -b -r t1.raw output/T1_tmp.cir'], shell=True)
     if label == 'drs4_get_analog':
         import subprocess
-        subprocess.run(['ngspice -b -r drs4_analog.raw paras/drs4_analog.cir'], shell=True)
+        subprocess.run(['ngspice -b -r drs4_analog.raw paras/circuitdrs4_analog.cir'], shell=True)
     if label == 'drs4_get_fig':
         from . import drs4_get_fig
         drs4_get_fig.main()
