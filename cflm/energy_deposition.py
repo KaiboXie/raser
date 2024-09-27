9b00600eeb7fe346a1c35b73741db867a6fb58c6
Yanpeng Li <lyp20001113@163.com>
2024-09-27 10:37:03 +0800

    Change for CFLM, Version:2

---
+++ cflm/energy_deposition.py
@@ -0,0 +1,36 @@
+import ROOT
+import json
+
+def getedep():
+
+    up = 25
+    low = 0
+    nbins = 25
+
+    geant4_json = "./raser/cflm/cflm.json"
+    with open(geant4_json) as f:
+         g4_dic = json.load(f)
+
+    file = ROOT.TFile(f"raser/cflm/output/{g4_dic['EdepBaseName']}")
+    tree = file.Get("cflm")
+    branch = tree.GetBranch("Edetector")
+
+    data = []
+
+    for event in tree:
+        value = branch.GetLeaf("Edetector").GetValue()
+        data.append(value)
+
+    histogram = ROOT.TH1F("histogram", "Histogram of Energy deposition", nbins, low, up)
+
+    histogram.SetXTitle("Energy deposition(MeV)")
+    histogram.SetYTitle("Events/{:.3f}".format((up - low) / nbins))
+
+    for value in data:
+        histogram.Fill(value)
+
+    c = ROOT.TCanvas("c", "c", 800, 600)
+
+    histogram.Draw()
+    c.SaveAs(f"raser/cflm/output/{g4_dic['EdepBaseName'].split('.')[0]}.png")
+
