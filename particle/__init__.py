fe60f08b6a8f077c7aebd548d08805c302971fb2
fuchenxi <1256257282@qq.com>
2024-10-06 22:38:25 +0800

    清除无用选项

--- particle/__init__.py
+++ particle/__init__.py
@@ -4,13 +4,8 @@ def main(kwargs):
     if label == 'temperature':
         from . import cal_temp
         cal_temp.main()
-
-    elif label == 'cflm_v1':
-        from . import cflm
-        cflm.main()
-    elif label == "test":
+    elif label == "energy_deposit":
         from . import g4_sic_energy_deposition
-        command="./param_file/g4macro/gui.mac"
         g4_sic_energy_deposition.main()
     else:
         raise NameError(label)
