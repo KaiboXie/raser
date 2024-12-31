164b4e5087144cd2445725b22d6ad885c457245e
Yanpeng Li <lyp20001113@163.com>
2024-12-31 14:24:28 +0800

    cflm 代码整理

--- cflm/__init__.py
+++ cflm/__init__.py
@@ -12,43 +12,20 @@ def main(kwargs):
     logging.info('This is INFO messaage')
     logging.debug('This is DEBUG messaage')
 
-    if label == 'cflm':
-        from . import cflm
-        cflm.main()
-    if label == 'GetSignal':
-        from . import get_signal
-        get_signal.main()
-    if label == 'GetVolCur':
-        from . import volt_current
-        volt_current.main()
-    if label == 'GetEdep':
-        from . import energy_deposition
-        energy_deposition.main()
-    if label == 'RootPlot_1D':
-        from . import sparticles_plot_1d
-        sparticles_plot_1d.main()
-    if label == 'GetTimeSignal':
-        from . import time_signal
-        time_signal.main()
-    if label == 'PoissonGen':
-         from . import poisson_generator
-         poisson_generator.main()
-    if label == 'GetSignalDsides':
-         from . import get_signal_dsides
-         get_signal_dsides.main()
-    if label == 'PoiGenDsides':
-         from . import poisson_generator_dsides
-         poisson_generator_dsides.main()
+    if label == 'GetDSPprep':
+        from . import cflm_p1
+        cflm_p1.main()
+    if label == 'GetDSPcurrent':
+         from . import get_current_p1
+         get_current_p1.main()
+    if label == 'GetDSPpoicurrent':
+        from . import poisson_generator_p1
+        poisson_generator_p1.main()
 
-    if label == 'GetPixelPrep':
-        from . import cflm_pixel_area
-        cflm_pixel_area.main()
-    if label == 'GetPixelEdep':
-        from . import get_pixel_area_edep
-        get_pixel_area_edep.main()
-    if label == 'GetPixelSignal':
-        from . import get_pixel_area_siganl
-        get_pixel_area_siganl.main()  
-    if label == 'GetPixelMaxSignal':
-        from . import get_pixel_area_maxsignal
-        get_pixel_area_maxsignal.main()
+    if label == 'GetCurrentP3_prep':
+        from . import cflm_p3
+        cflm_p3.main()
+    if label == 'GetCurrentP3':
+        from. import poisson_generator_p3
+        poisson_generator_p3.main()
+
