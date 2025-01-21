ee59dc751b90aa3aed8a70faa3aeb84f004f2961
fuchenxi <1256257282@qq.com>
2025-01-22 06:12:21 +0800

    时间分辨模块批量提交作业

--- util/batchjob.py
+++ util/batchjob.py
@@ -15,18 +15,17 @@ import pwd
 
 from util.output import create_path
 
-def main(destination_subfolder, command, batch_number, args):
-    test = vars(args)['test'] 
+def main(destination_subfolder, command, batch_level, is_test):
     stat_info = os.stat("./")
     uid = stat_info.st_uid
     gid = stat_info.st_gid
     user = pwd.getpwuid(uid)[0]
     group = grp.getgrgid(gid)[0]
     
-    if batch_number < 0:
-        mem = 10 * (-batch_number)
-    elif batch_number > 0:
-        mem = 5000 * batch_number
+    if batch_level < 0:
+        mem = 10 * (-batch_level)
+    elif batch_level > 0:
+        mem = 5000 * batch_level
  
     create_path("./output/{}/jobs".format(destination_subfolder))
     command_name = command.replace(" ","_").replace("/","_")
@@ -44,7 +43,7 @@ def main(destination_subfolder, command, batch_number, args):
                 + IMGFILE + " " \
                 + "python3 -mdevsim.umfpack.umfshim raser/__main__.py"
     gen_job(jobfile_name, run_code=raser_shell+' '+command)
-    submit_job(jobfile_name, destination_subfolder, group, mem, test=test)
+    submit_job(jobfile_name, destination_subfolder, group, mem, is_test=is_test)
 
 def gen_job(jobfile_name, run_code):
     jobfile = open(jobfile_name, "w")
@@ -52,15 +51,15 @@ def gen_job(jobfile_name, run_code):
     jobfile.close()
     print("Generate job file: ", jobfile_name)
 
-def submit_job(jobfile_name, destination_subfolder, group, mem, test=False):
+def submit_job(jobfile_name, destination_subfolder, group, mem, is_test=False):
     print("Submit job file: ", jobfile_name)
     os.chmod(jobfile_name, 0o755)
     command = "hep_sub -o ./output/{}/jobs -e ./output/{}/jobs {} -mem {} -g {}".format(
         destination_subfolder, destination_subfolder, jobfile_name, mem, group)
-    run_cmd(command, test)
+    run_cmd(command, is_test)
 
-def run_cmd(command, test=False):
-    if test:
+def run_cmd(command, is_test=False):
+    if is_test:
         sys.stdout.write(command+'\n')
         return 
     subprocess.run([command],shell=True)
