45fc5207f5af0774a298ceb3c9be7d04337baf45
Chenxi Fu <1256257282@qq.com>
2025-03-31 09:03:04 +0000

    Revert "代码库形式调整"
    This reverts commit 1d2cf959a89f0c7c61824818b7f1f63cdf346351

--- setting/detector/__main__.py
+++
@@ -1,7 +0,0 @@
-import os
-import subprocess
-import sys
-
-device = sys.argv[1]
-command = os.getenv("RASER_SETTING_PATH")+"/detector/"+device+".py"
-subprocess.run(["python3"+' '+command], shell=True, executable='/bin/bash')
